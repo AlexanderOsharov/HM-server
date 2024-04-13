5ae8726acb5d0ddc88faa71eac16dee4b1a13863
Axtemka <115342104+Axtemka@users.noreply.github.com>
2024-04-13 11:30:39 +0300

    HM-server-code

--- HistoryMomentServer/main.py
+++ HistoryMomentServer/main.py
@@ -1,25 +1,27 @@
 import io
 import json
+import time
+
 import firebase_admin
 from firebase_admin import credentials, db
-
 
 from download import download_and_extract_archive, process_json_files
 
 url = "https://opendata.mkrf.ru/opendata/7705851331-egrkn/data-51-structure-6.json.zip?e={%22attachment%22:true}"
 
-download_and_extract_archive(url)
-data = process_json_files("temp")
+while True:
+    download_and_extract_archive(url)
+    data = process_json_files("temp")
 
-with io.open("moscow_objects.json", "w", encoding='utf-8') as file:
-    json.dump(data, file, ensure_ascii=False)
+    with io.open("moscow_objects.json", "w", encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False)
 
+    cred = credentials.Certificate("credentials.json")
+    firebase_admin.initialize_app(cred, {"databaseURL": "https://historymomentdatabase-default-rtdb.firebaseio.com/"})
 
-cred = credentials.Certificate("credentials.json")
-firebase_admin.initialize_app(cred, {"databaseURL": "https://historymomentdatabase-default-rtdb.firebaseio.com/"})
+    ref = db.reference("/objects")
 
-ref = db.reference("/objects")
-
-with io.open("moscow_objects.json", "r", encoding='utf-8') as file:
-    json_data = json.load(file)
-    ref.set(json_data)
+    with io.open("moscow_objects.json", "r", encoding='utf-8') as file:
+        json_data = json.load(file)
+        ref.set(json_data)
+    time.sleep(86400)
